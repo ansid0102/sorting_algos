d2503e48e03185125a9a0b21ced9f50b43237c0c
ansid0102 <harikanchiraju@hotmail.com>
2023-02-20 18:36:13 +0530

    Bubble Sort added

---
+++ BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class BubbleSort {
+	public static void main(String[] args){
+	int arr[] = {7,6,5,4,3,2,1};
+
+	int n = arr.length;
+
+	for(int i=0;i<n;i++){
+	    
+	    for(int j=0;j<n-i-1;j++){
+
+		    if(arr[j] > arr[j+1]){
+
+			    int temp = arr[j];
+
+			    arr[j]=arr[j+1];
+
+			    arr[j+1]=temp;
+		    }
+	    }
+	}
+
+	System.out.println(Arrays.toString(arr));
+}
+}
